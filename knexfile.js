51927fff4ed45f79cb593e0f9c615fc59c3b5451
Aidan Baack <aebaack@gmail.com>
2017-01-13 16:29:34 -0700

    Add knex

--- knexfile.js
+++ knexfile.js
@@ -1,2 +1,9 @@
 'use strict';
 
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/lazy_buffs'
+  }
+  // Add more environments here, I'm short on time
+};
