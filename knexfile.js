0d84804780d9270c82401d8cfb3432c956bb678b
KaneLabs <ryankane28@gmail.com>
2017-01-14 01:24:26 -0700

    deploy test

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,18 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/buffs'
-  }
+    connection: 'postgres://localhost/buffs',
+  },
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  },
   // Add more environments here, I'm short on time
 };
