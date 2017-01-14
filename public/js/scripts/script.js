bc312d00f74bacc6a7696acf80ce9835793ac715
Aidan Baack <aebaack@gmail.com>
2017-01-13 18:34:37 -0700

    Add show and hide

--- public/js/scripts/script.js
+++ public/js/scripts/script.js
@@ -1,5 +1,13 @@
 'use strict';
 
 $(document).ready(() => {
-  
+  $('#parterButton').click(() => {
+    $('#partnerInfo').hide();
+    $('#partnerForm').show();
+  });
+
+  $('#driverButton').click(() => {
+    $('#driverInfo').hide();
+    $('#driverForm').show();
+  });
 });
